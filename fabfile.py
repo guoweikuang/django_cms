c98ac02161eb9d8a7005315f7ac39e287225bff7
guoweikuang <673411814@qq.com>
2017-09-27 17:25:18 +0800

    remove fabfile.py

--- fabfile.py
+++ fabfile.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from fabric.api import env, run
+from fabric.api import env, run, cd
 from fabric.operations import sudo
 
 GIT_REPO = "https://github.com/guoweikuang/django_cms.git"
@@ -7,19 +7,20 @@ GIT_REPO = "https://github.com/guoweikuang/django_cms.git"
 env.user = 'root'
 env.password = 'GUOweikuang2017!'
 
-env.hosts = ['120.25.95.55']
+env.hosts = ['demo.guoweikuang.com']
 env.port = '22'
 
 
 def deploy():
-    source_folder = '/home/guoweikuang/project/'
+    source_folder = '/home/guoweikuang/project/django_cms'
 
-    run('cd %s && git clone %s' % (source_folder, GIT_REPO))
+    run('cd %s && git pull ' % source_folder)
     run("""
         cd {} &&
         /home/guoweikuang/project/venv/bin/pip install -r requirements.txt &&
         /home/guoweikuang/project/venv/bin/python3 manage.py collectstatic --noinput &&
         /home/guoweikuang/project/venv/bin/python3 manage.py migrate
-        """.format(source_folder + 'django_cms'))
-    sudo('/home/guoweikuang/project/venv/bin/python3 manage.py runserver 0.0.0.0:8009')
+        """.format(source_folder))
+    with cd(source_folder):
+        sudo('/home/guoweikuang/project/venv/bin/python3 manage.py runserver 0.0.0.0:8009')
     
